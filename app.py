bdf84e5c7add5786cae355bc2dc68cb61d32d538
Silvio <silviolima07@gmail.com>
2026-03-21 18:04:31 -0300

    Added recomendacao

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ html_page_title = """
                """               
 st.markdown(html_page_title, unsafe_allow_html=True)
 
-st.info("Este conteúdo é destinado apenas a fins educacionais. Os dados exibidos são ilustrativos e podem não corresponder a situações reais.")
+st.info("Este conteúdo é destinado apenas a fins educacionais.")
+st.info("Os dados exibidos são ilustrativos e podem não corresponder a situações reais.")
 
 # Carregar o modelo (com cache para não recarregar toda vez)
 @st.cache_resource
